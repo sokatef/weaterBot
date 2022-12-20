4070838b893b57239315552701d976b308f5bead
SergeyLox54 <121050816+SergeyLox54@users.noreply.github.com>
2022-12-20 10:40:19 +0500

    Update main.py

--- main.py
+++ main.py
@@ -17,3 +17,30 @@ def getCurrentWeater(city):
         if (currentWeaterResponse.status_code==400 and currentWeaterResponse.json()['error']['code']=='406'):
             return 'Invalid location'
         else: return 'Bad request'
+def getWeatherForNextThreeDays(city):
+    queryParams={"key": weatherApiTpken, "q": city, "days": 3}
+    currentWeaterResponse=requests.get(weatherApiEndpoint+'forecast.json',queryParams)
+    if currentWeaterResponse.status_code==200:         
+            a = {"day1":{"date":currentWeaterResponse.json()['forecast']['forecastday'][0]['date'],
+                     "averageTemp":currentWeaterResponse.json()['forecast']['forecastday'][0]['day']['avgtemp_c'],
+                     "averageWindSpeed":currentWeaterResponse.json()['forecast']['forecastday'][0]['day']['maxwind_kph'],
+                     "averageHumidity":currentWeaterResponse.json()['forecast']['forecastday'][0]['day']['avghumidity'],
+                     "chanceOfRain":currentWeaterResponse.json()['forecast']['forecastday'][0]['day']['daily_chance_of_rain'],
+                     "chanceOfSnow":currentWeaterResponse.json()['forecast']['forecastday'][0]['day']['daily_chance_of_snow']},
+                 "day2":{"date":currentWeaterResponse.json()['forecast']['forecastday'][1]['date'],
+                     "averageTemp":currentWeaterResponse.json()['forecast']['forecastday'][1]['day']['avgtemp_c'],
+                     "averageWindSpeed":currentWeaterResponse.json()['forecast']['forecastday'][1]['day']['maxwind_kph'],
+                     "averageHumidity":currentWeaterResponse.json()['forecast']['forecastday'][1]['day']['avghumidity'],
+                     "chanceOfRain":currentWeaterResponse.json()['forecast']['forecastday'][1]['day']['daily_chance_of_rain'],
+                     "chanceOfSnow":currentWeaterResponse.json()['forecast']['forecastday'][1]['day']['daily_chance_of_snow']},
+                 "day3":{"date":currentWeaterResponse.json()['forecast']['forecastday'][2]['date'],
+                     "averageTemp":currentWeaterResponse.json()['forecast']['forecastday'][2]['day']['avgtemp_c'],
+                     "averageWindSpeed":currentWeaterResponse.json()['forecast']['forecastday'][2]['day']['maxwind_kph'],
+                     "averageHumidity":currentWeaterResponse.json()['forecast']['forecastday'][2]['day']['avghumidity'],
+                     "chanceOfRain":currentWeaterResponse.json()['forecast']['forecastday'][2]['day']['daily_chance_of_rain'],
+                     "chanceOfSnow":currentWeaterResponse.json()['forecast']['forecastday'][2]['day']['daily_chance_of_snow']}}
+            return a
+    else:
+        if (currentWeaterResponse.status_code==400 and currentWeaterResponse.json()['error']['code']=='406'):
+            return 'Invalid location'
+        else: return 'Bad request'
